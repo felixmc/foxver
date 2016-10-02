ccbd3639ce744c81510b36cb568d102dfbd53ec4
Felix Milea-Ciobanu <felixmilea@gmail.com>
2016-10-02 09:19:33 -0600

    tweaks

---
+++ bin/generate_app.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+mkdir -p 'src/app/controllers'
+mkdir -p 'src/app/models'
+mkdir -p 'src/app/providers'
+mkdir -p 'src/app/services'
+mkdir -p 'src/app/hooks'
+mkdir -p 'src/lib/'
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-console.log('Initializing server..') // eslint-disable-line no-console
+export default function initServer (appDir) {
+  console.log('Initializing server..') // eslint-disable-line no-console
 
-const Foxver = require('../lib')
-const app = new Foxver()
+  const Foxver = require('../lib')
+  const app = new Foxver(appDir)
 
-app.init()
-  .then(App => {
-    Log.info('Init OK')
-    App.start()
-  })
-  .catch(err => {
-    console.error('Init failed:', err) // eslint-disable-line no-console
-  })
+  app.init()
+    .then(App => {
+      Log.info('Init OK')
+      App.start()
+    })
+    .catch(err => {
+      console.error('Init failed:', err) // eslint-disable-line no-console
+    })
+
+  return app
+}
